22eb17d7956ac9a32acee151636016724e6209c9
Saida Yurishi <syurishi@brightcove.com>
2021-10-11 16:34:22 -0500

    adding HelloWorld

---
+++ src/javaLeccion1/GrossPayCalculator.java
@@ -0,0 +1,7 @@
+package javaLeccion1;
+
+public class GrossPayCalculator {
+    public static void main(String arg[]){
+        
+    }
+}
---
+++ src/javaLeccion1/hellloWorld.java
@@ -0,0 +1,7 @@
+package javaLeccion1;
+
+public class hellloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
