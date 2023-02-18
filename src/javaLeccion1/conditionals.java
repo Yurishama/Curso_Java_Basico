20ae3d876d7e976f2dd9dbf1b3f88fb914fcc164
Saida Reyes <yurishama@gmail.com>
2023-02-17 18:03:51 -0600

    variables and conditionals

---
+++ src/javaLeccion1/conditionals.java
@@ -0,0 +1,13 @@
+package javaLeccion1;
+
+public class conditionals {
+    public static void main(String[] args){
+
+        int a = 4;
+        boolean b = a == 4;
+
+        if (b){
+            System.out.println("this is true");
+        }
+    }
+}
--- src/javaLeccion1/variablesyTipos.java
+++ src/javaLeccion1/variablesyTipos.java
@@ -2,6 +2,14 @@ package javaLeccion1;
 
 public class variablesyTipos {
     public static void main(String[] args){
+
+        String var1 = "This is a string variable";
+        System.out.println(var1);
+
+        Integer var2 = 21;
+        int var3 = 8;
+        System.out.println("This variables ar Numbers: "+ var2 + var3);
+
         //number, 1 byte
         byte zero=0;
         System.out.println("byte :" + zero);
